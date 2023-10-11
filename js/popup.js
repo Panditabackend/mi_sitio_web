551fba30b9a01886e3f4333e5f18d259ac043c03
Clarivel Barrios <claribel_911@hotmail.es>
2023-10-10 20:45:12 -0500

    mi sitio web

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+// popup.js con jQuery
+$(document).ready(function() {
+    // Obtén una referencia al formulario y al botón "Enviar"
+    var form = $("form");
+    var submitButton = $("button[type='submit']");
+
+    // Agrega un evento al formulario para capturar el envío
+    form.on("submit", function(event) {
+        event.preventDefault(); // Previene el envío del formulario
+        // Muestra el popup de confirmación utilizando jQuery
+        $("#confirmationModal").modal("show");
+    });
+});
